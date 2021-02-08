8685dbc526dfb2234593c5c7c288612dba155c34
Juan Manuel Charchalac <15004342@galileo.edu>
2021-02-07 21:51:43 -0600

    Primer commit

---
+++ Meses_15004342.js
@@ -0,0 +1,70 @@
+var mes=10
+switch(mes) {
+  case 1:
+    alert("Enero");
+    break;
+  case 2:
+    alert("Febrero");
+    break;
+  case 3:
+    alert("Marzo");
+    break;
+  case 4:
+    alert("Abril");
+    break;
+  case 5:
+    alert("Mayo");
+    break;
+  case 6:
+    alert("Junio");
+    break;
+  case 7:
+    alert("Julio");
+    break;
+  case 8:
+    alert("Agosto");
+    break;
+  case 9:
+    alert("Septiembre");
+    break;
+  case 10:
+    alert("Octubre");
+    break;
+  case 11:
+    alert("Noviembre");
+    break;
+  case 12:
+    alert("Diciembre");
+    break;
+  default:
+    alert("Numero no valido")
+    break;
+}if(mes==1){
+  alert("Es Invierno");
+}if(mes==2){
+  alert("Es Invierno");
+}if(mes==3){
+  alert("Es Primavera");
+}if(mes==4){
+  alert("Es Primavera");
+}if(mes==5){
+  alert("Es Primavera");
+}if(mes==6){
+  alert("Es Verano");
+}if(mes==7){
+  alert("Es Verano");
+}if(mes==8){
+  alert("Es Verano");
+}if(mes==9){
+  alert("Es Otoño");
+}if(mes==10){
+  alert("Es Otoño");
+}if(mes==11){
+  alert("Es Otoño");
+}if(mes==12){
+  alert("Es Invierno");
+}if(mes==13){
+  alert(" ")
+}else{
+  alert("Carnet 15004342. Juan Manuel Charchalac")
+}
